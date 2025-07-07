60bc7326db160957a4ffed3c0a9ca909a9099321
nagalakshmi08 <lakshmireddydwarampudi436@gmail.com>
2025-07-07 11:05:56 +0530

    Connected prisma to postgresql

--- prisma/migrations/20250505110516_api_credentials/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ApiCredentials" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "token" TEXT NOT NULL,
-    "baseUrl" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
